6443b521bcfebdc0a6d8a8fbbc69e25d4dab9935
frank <franksavena@gmail.com>
2022-11-12 15:45:33 -0600

    add event listener in JS

---
+++ index.js
@@ -0,0 +1,13 @@
+// ID selectors 
+const form = document.querySelector('#Recipe-search')
+
+
+// form event listener 
+
+form.addEventListener('submit' , (e) => {
+    e.preventDefault()
+    console.log('Hello')
+
+    e.target
+})
+
