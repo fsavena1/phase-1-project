68a311a7d76b7429ba27a29dcf28dd1217d2b6fe
John Ly <johnly0422@gmail.com>
2022-11-14 16:27:33 -0500

    new js code and css styling

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 // ID selectors 
 const form = document.querySelector('#Recipe-search')
 const newSearch = document.querySelector('#recipe-results')
+const navBar = document.querySelector('#recipe-bar')
+const recipeName = document.createElement('h1')
+const newRecipe = document.createElement('div')
+const recipeImg = document.createElement('img')
+
 
 const api_ID = 'dd405108'
 const api_Key ='48fcae0e99aa225862af0bcad8cadbc5'
@@ -14,12 +19,42 @@ form.addEventListener('submit' , (e) => {
     e.preventDefault()
     // console.log('Hello')
      let formInput = e.target['name'].value
-    console.log(formInput)
 
     fetch(`https://api.edamam.com/search?q=${formInput}&app_id=${api_ID}&app_key=${api_Key}`)
     .then(res=>res.json())
-    .then(data=> console.log(data))
+    .then(recipes => {
+        while(navBar.firstChild) {
+            navBar.removeChild(navBar.firstChild)
+        }
 
+        recipes.hits.forEach(recipe => {
+           
+
+            const newSpan = document.createElement('span')
+            const image = document.createElement('img')
+
+            newSpan.className = 'span-tile'
+
+            image.src = recipe.recipe.image
+            image.className = 'bar-image'
+
+            newSpan.append(image)
+            navBar.append(newSpan)
+            navBar.className = 'recipe-bar'
+
+            newSpan.addEventListener('click', () => {
+                console.log('hello')
+                newRecipe.className = "card"
+                recipeName.textContent = recipe.recipe.label
+
+                recipeImg.classList.add('detail-image')
+                recipeImg.src = recipe.recipe.image
+
+                newRecipe.append(recipeName,recipeImg)
+                newSearch.append(newRecipe)
+            })
+        })        
+    })
 })
 
 // creating recipe elements 
@@ -27,27 +62,27 @@ form.addEventListener('submit' , (e) => {
 
 // function createRecipe(recipe) {
 
-//     const newRecipe = document.createElement('div')
-//     newRecipe.className = "card"
+    // const newRecipe = document.createElement('div')
+    // newRecipe.className = "card"
 
-//     const recipeName = document.createElement('h1')
-//     recipeName.textContent = 
+    // const recipeName = document.createElement('h1')
+    // recipeName.textContent = 
 
-//     const recipeImg = document.createElement('img')
-//     recipeImg.src = 
+    // const recipeImg = document.createElement('img')
+    // recipeImg.src = 
 
-//     const recipeDes = document.createElement('p')
-//     recipeDes.textContent = 
-//     const r
-
-
-
-//     newRecipe.append(recipeName)
-//     newRecipe.append(recipeImg)
-//     newRecipe.append(recipeDes)
+    // const recipeDes = document.createElement('p')
+    // recipeDes.textContent = 
+    // const r
 
 
-//     newSearch.append(newRecipe)
+
+    // newRecipe.append(recipeName)
+    // newRecipe.append(recipeImg)
+    // newRecipe.append(recipeDes)
+
+
+    // newSearch.append(newRecipe)
 
 // }
 
